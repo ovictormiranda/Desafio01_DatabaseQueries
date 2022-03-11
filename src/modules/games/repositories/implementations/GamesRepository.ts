9429d0641bff53ca167957510498fe2c44b2e1c8
Miranda <omirandavictor@gmail.com>
2022-03-10 21:05:52 -0300

    Desafio concluido com sucessogit push origin main!

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -16,19 +16,17 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
       .where('title ILIKE :title', { title: `%${param}%`})
       .getMany();
     }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query('SELECT COUNT(id) FROM games'); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(id) FROM games'); 
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
       .relation(Game, "users")
       .of(id)
       .loadMany();
